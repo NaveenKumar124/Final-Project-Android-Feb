c5b18c6413344ad0456c6e65c315c4cb4d2626d5
navimalhotra <navimalhotra124@gmail.com>
2020-02-15 17:27:14 -0500

    Setting layout , adding custom toolbar and RecyclerView.

--- app/src/main/java/com/example/finalproject_feb/MainActivity.java
+++ app/src/main/java/com/example/finalproject_feb/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.finalproject_feb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.listOfNotes);
     }
 }
