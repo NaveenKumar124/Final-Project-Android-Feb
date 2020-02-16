ffc6313b90fa2d50d59e5a3e6a9027b84fe47a8e
navimalhotra <navimalhotra124@gmail.com>
2020-02-16 13:50:24 -0500

    Title Bar.

--- app/src/main/java/com/example/finalproject_feb/AddNote.java
+++ app/src/main/java/com/example/finalproject_feb/AddNote.java
@@ -3,24 +3,49 @@ package com.example.finalproject_feb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.Calendar;
 
 public class AddNote extends AppCompatActivity {
 
     Toolbar toolbar;
     EditText noteTitle, noteDetails;
+    Calendar c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        //getSupportActionBar().setTitle("New Note");
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         noteTitle = findViewById(R.id.noteTitle);
         noteDetails = findViewById(R.id.noteDetails);
+
+        noteTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() != 0){
+                    getSupportActionBar().setTitle(s);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
