59b75bbad07007d33f7e9ca8e9eb42a922c3e196
navimalhotra <navimalhotra124@gmail.com>
2020-02-16 13:15:34 -0500

    After adding fields for note title and note details and setting constraints for them.

--- app/src/main/java/com/example/finalproject_feb/AddNote.java
+++ app/src/main/java/com/example/finalproject_feb/AddNote.java
@@ -1,24 +1,26 @@
 package com.example.finalproject_feb;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class AddNote extends AppCompatActivity {
 
     Toolbar toolbar;
-    RecyclerView recyclerView;
+    EditText noteTitle, noteDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_note);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        recyclerView = findViewById(R.id.listOfNotes);
+        noteTitle = findViewById(R.id.noteTitle);
+        noteDetails = findViewById(R.id.noteDetails);
     }
 }
